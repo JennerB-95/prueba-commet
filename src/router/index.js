2d5b5d7f444d1601d2b0a6affbf3cfcf4091839a
JennerB-95 <jbamacg@miumg.edu.gt>
2023-03-17 18:29:47 -0600

    Genesis commit

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/auth/MyLogin.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'login',
+    name: 'homepage'
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
